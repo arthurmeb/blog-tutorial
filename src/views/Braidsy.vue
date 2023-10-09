05d4e927bc75cb72a690b9ce443be70c735ddca9
arthurmeb <120094553+arthurmeb@users.noreply.github.com>
2023-10-09 18:48:13 +0100

    composable1

--- src/views/Braidsy.vue
+++ src/views/Braidsy.vue
@@ -2,26 +2,26 @@
     <H1>Braidsy.com</H1>
     <PostList :posts="posts" v-if="showPosts"/>
     <button @click="togglePosts">Toggle posts</button>
+    <div v-if="error">{{ error }}</div>
+
 </template>
 
 <script>
-import { ref } from 'vue'
+
 import PostList from '@/components/PostList.vue'
 
 export default {
     name: 'Braidsy',
     components: {PostList},
     setup() {
-        const posts = ref([
-            {title: 'Post 1', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', id: 1},
-            {title: 'Post 2', body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem', id: 2},
-            
-        ])
+       
+
+        load()
 
         let showPosts = ref(true)
 
         let togglePosts = () => {showPosts.value = !showPosts.value}
-        return { posts, showPosts, togglePosts}
+        return { posts, showPosts, togglePosts, error, load}
     }
 }
 </script>
