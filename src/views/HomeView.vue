b71b19d549cd695d03a48b11105ba2557f903ebd
arthurmeb <120094553+arthurmeb@users.noreply.github.com>
2023-10-09 02:38:53 +0100

    ?

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,10 +6,16 @@
     <p v-if="age"> My name is {{ age }}</p>
     <input type="text" v-model="status">
   </div>
+  <br> 
+  <input type="text" v-model="search">
+  <p> Search term: {{ search }}</p>
+  <div v-for="style in searchName" :key="style">{{ style }}</div>
+  <button @click="handleWatch">Stop watching</button>
+
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 
 export default {
@@ -19,13 +25,28 @@ export default {
       let status = ref('working')
       let age = ref(false)
 
+      const search = ref('')
+
+      let styles = ref(['cornrows', 'twists', 'barrel twists', 'plaits', 'two strands'])
+      const searchName = computed(() => {
+        return styles.value.filter((style) => style.includes(search.value))
+      })
+
       const uhh = ref(null)
 
       console.log(age, comp, status)
 
-      const handleClick = () => {age.value = 33, console.log(age) }
+      const handleClick = () => {age.value = 33, console.log(age.value) }
 
-      return {age: age, comp, status: status, handleClick, uhh}
+      const watcher = watch(search, () => {console.log('watch ran')})
+      
+      const handleWatch = () => {
+        return watcher()
+      }
+
+      return {age: age, comp, status: status, handleClick, uhh, styles, searchName, search, watcher, handleWatch}
+
+      
     }
 
 
